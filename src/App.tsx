6e00c16c2ff8fc3f3d16ae3fcc0469c6a3ba9672
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-07-20 15:28:15 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from "axios"
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import FacebookLogin, { } from '@greatsumini/react-facebook-login';
+import { FacebookLoginClient } from '@greatsumini/react-facebook-login';
+
+
+const clientId = "674406797855-n6am3392nlr8dvlmugf3cepu8p3rs8fg.apps.googleusercontent.com"
+
+type Props = {
+  onClick: any
+  onLogoutClick: any
+}
+
+const CustomComponent  = ({onClick, onLogoutClick}: Props) => (
+  <>
+    <button onClick={onClick}>Login with Facebook</button>
+    <button onClick={onLogoutClick(function (response) {
+      // if ('connected' === response.status) {
+      //   console.log("Logout")
+      //   window.location.href = '/';
+      // } else {
+      //   console.log('There has been an error.');
+      // }
+    }, {
+      access_token: "EAAMTswOcD7sBAAJVw635pLlkRjVBJEgVXNna2pDco2wEDyNbWPYqqIdBpJvCzVbNAOQLguf85ZCp1lLeG2m3jho2Xy3TAkU7QLZCfpRIMLoReZBvaMmT4SjX6UZALKa80bNW3d3qUK1OZCfQC6BwbyhHccaxejViOXe5Kc15TTJxRr5KEY51NnaE9iF6QdZAT2GL8gIPDtplU0OjOUX19sIrbLHjzsrR8ZD"
+    })}>Logout</button>
+  </>
+)
+
+// console.log(window.FB)
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+  const [accessToken, setAccessToken] = useState("")
+
+  //   const handleGoogleLogin = async () => {
+  //     try {
+  //       const res = await axios.get("http://localhost:4000/auth/google/login")
+  //       console.log({ res })
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+
+  //   FB.getLoginStatus(function(response) {
+  //     statusChangeCallback(response);
+  // });
+
+  async function login() {
+    FacebookLoginClient.login((res) => {
+      console.log(res);
+    });
+}
+
+function logout() {
+  // revoke app permissions to logout completely because FB.logout() doesn't remove FB cookie
+  FacebookLoginClient.logout((res) => {
+    console.log('logout completed!', res);
+  });
+}
+return (
+  <>
+
+    <FacebookLogin
+      appId="866084510764987"
+      onSuccess={(response) => {
+        console.log('Login Success!', response);
+      }}
+      onFail={(error) => {
+        console.log('Login Failed!', error);
+      }}
+      onProfileSuccess={(response) => {
+        console.log('Get Profile Success!', response);
+      }}
+    // render={({ onClick, logout }) => (
+    //   <CustomComponent onClick={onClick} onLogoutClick={logout} />
+    // )}
+    />
+    <button onClick={login}>Login</button>
+    <button onClick={logout}>Logout</button>
+  </>
+)
+}
+
+export default App
---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+import {FB} from "@greatsumini/react-facebook-login"
+
+// declare global {
+//     interface Window { FB: FB; }
+// }
